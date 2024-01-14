9ec5c3665d7c02e230fe3a677933b2e4507c4ac6
Jack <jack282323@gmail.com>
2024-01-14 16:37:38 +0100

    Added GA

--- src/app/bdr/page.tsx
+++ src/app/bdr/page.tsx
@@ -2,80 +2,97 @@
 import { Textarea } from "@/components/ui/textarea";
 import { useForm, ValidationError } from "@formspree/react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 
 export default function ContactForm() {
   const [state, handleSubmit] = useForm("xkndnlar");
 
-  if (state.succeeded) {
-    return <p>Thanks for your submission!</p>;
-  }
+  const Header = () => {
+    const router = useRouter();
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="text-black text-lg">
-        How do you improve your sales skills?
-        <Textarea className="mt-4" placeholder="Enter your response here..." />
-        <div className="text-black text-lg mt-10">
-          Describe your best achievement in B2B sales.
+    if (state.succeeded) {
+      return <p>Thanks for your submission!</p>;
+    }
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <header className="mr-5">
+          {router.pathname !== "/" && (
+            <button onClick={() => router.back()}>Back</button>
+          )}
+        </header>
+        <div className="text-black text-lg">
+          How do you improve your sales skills?
           <Textarea
             className="mt-4"
             placeholder="Enter your response here..."
-            id="message"
-            name="message"
           />
-          <ValidationError
-            prefix="Message"
-            field="message"
-            errors={state.errors}
-          />
+          <div className="text-black text-lg mt-10">
+            Describe your best achievement in B2B sales.
+            <Textarea
+              className="mt-4"
+              placeholder="Enter your response here..."
+              id="message"
+              name="message"
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+          </div>
+          <div className="text-black text-lg mt-10">
+            What drives you to succeed in sales?
+            <Textarea
+              className="mt-4"
+              placeholder="Enter your response here..."
+              id="message2"
+              name="message2"
+            />
+          </div>
+          <div className="text-black text-lg mt-10">
+            Is learning important in B2B sales? Why?
+            <Textarea
+              className="mt-4"
+              placeholder="Enter your response here..."
+              id="message3"
+              name="message3"
+            />
+          </div>
+          <div className="text-black text-lg mt-10">
+            How would you persuade someone to complete a project using your
+            strategies?
+            <Textarea
+              className="mt-4"
+              placeholder="Enter your response here..."
+              id="message4"
+              name="message4"
+            />
+          </div>
+          <div className="text-black text-lg mt-10">
+            Please provide your email to receive your score
+            <Textarea
+              className="mt-4"
+              placeholder="sample@sample.com"
+              id="email"
+              name="email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </div>
+          <Button
+            className="bg-blue-300 flex float-right mx-5 my-2"
+            variant="outline"
+            disabled={state.submitting}
+          >
+            Submit
+          </Button>
         </div>
-        <div className="text-black text-lg mt-10">
-          What drives you to succeed in sales?
-          <Textarea
-            className="mt-4"
-            placeholder="Enter your response here..."
-            id="message2"
-            name="message2"
-          />
-        </div>
-        <div className="text-black text-lg mt-10">
-          Is learning important in B2B sales? Why?
-          <Textarea
-            className="mt-4"
-            placeholder="Enter your response here..."
-            id="message3"
-            name="message3"
-          />
-        </div>
-        <div className="text-black text-lg mt-10">
-          How would you persuade someone to complete a project using your
-          strategies?
-          <Textarea
-            className="mt-4"
-            placeholder="Enter your response here..."
-            id="message4"
-            name="message4"
-          />
-        </div>
-        <div className="text-black text-lg mt-10">
-          Please provide your email to receive your score
-          <Textarea
-            className="mt-4"
-            placeholder="sample@sample.com"
-            id="email"
-            name="email"
-          />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-        </div>
-        <Button
-          className="bg-blue-300 flex float-right mx-5 my-2"
-          variant="outline"
-          disabled={state.submitting}
-        >
-          Submit
-        </Button>
-      </div>
-      <ValidationError errors={state.errors} />
-    </form>
-  );
+        <ValidationError errors={state.errors} />
+      </form>
+    );
+  };
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import GoogleAnalytics from "./components/Analytics";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+        <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+      ) : null}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
       <body
         className={cn("relative h-full antialiased font-sans", inter.className)}
       >
