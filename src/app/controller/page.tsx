ebbff9486337780030ea462f93a4072e40146e61
Jack <jack282323@gmail.com>
2024-01-15 17:19:22 +0100

    Adding form pages

---
+++ src/app/controller/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm, ValidationError } from "@formspree/react";
+import { Button } from "@/components/ui/button";
+
+export default function ContactForm() {
+  const [state, handleSubmit] = useForm("mdoqowgv");
+
+  if (state.succeeded) {
+    return <p>Thanks for your submission!</p>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="text-black text-lg">
+        1. How would you best differentiate your competencies as a controller
+        and why do you think these skills make you the perfect candidate for
+        this specific position?
+        <Textarea className="mt-4" placeholder="Enter your response here..." />
+        <div className="text-black text-lg mt-10">
+          2. What&aposs your strategy for simplifying and explaining a complex
+          financial report to someone who needs help understanding it?
+          <Textarea
+            className="mt-4"
+            placeholder="Enter your response here..."
+            id="message"
+            name="message"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <div className="text-black text-lg mt-10">
+          3. Describe what you believe are some of the best strategies for
+          increasing a company&aposs revenue.
+          <Textarea
+            className="mt-4"
+            placeholder="Enter your response here..."
+            id="message2"
+            name="message2"
+          />
+        </div>
+        <div className="text-black text-lg mt-10">
+          What process do you use when auditing financial reports?
+          <Textarea
+            className="mt-4"
+            placeholder="Enter your response here..."
+            id="message3"
+            name="message3"
+          />
+        </div>
+        <div className="text-black text-lg mt-10">
+          Explain a time where your work led you to uncover a potential
+          inefficiency or opportunity in the business. How did you work to
+          communicate and implement this into commercial functions?
+          <Textarea
+            className="mt-4"
+            placeholder="Enter your response here..."
+            id="message4"
+            name="message4"
+          />
+        </div>
+        <div className="text-black text-lg mt-10">
+          Behavioral: Have you ever been involved in ethical situations where
+          your role was conflicted by different goals placed on you?
+          <Textarea
+            className="mt-4"
+            placeholder="Enter your response here..."
+            id="message5"
+            name="message5"
+          />
+        </div>
+        <div className="text-black text-lg mt-10">
+          Please offer any feedback you have for the site
+          <Textarea
+            className="mt-4"
+            placeholder="Feedback..."
+            id="message6"
+            name="message6"
+          />
+        </div>
+        <div className="text-black text-lg mt-10">
+          Please provide your email to receive your score
+          <Textarea
+            className="mt-4"
+            placeholder="sample@sample.com"
+            id="email"
+            name="email"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <Button
+          className="bg-blue-300 flex float-right mx-5 my-2"
+          variant="outline"
+          disabled={state.submitting}
+        >
+          Submit
+        </Button>
+      </div>
+      <ValidationError errors={state.errors} />
+    </form>
+  );
+}
