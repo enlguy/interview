3d176c8fa35418a9d1db19ef43b0d66d26dc94eb
Jack <jack282323@gmail.com>
2024-01-15 17:09:04 +0100

    Add logo to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ import {
 export default function Home() {
   return (
     <MaxWidthWrapper>
+      <div className="ml-1">
+        <Image src="/interprep2.svg" width={200} height={200} alt="logo" />
+      </div>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Prepare for your job interview,{" "}
